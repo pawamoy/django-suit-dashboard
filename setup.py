a3df8052ffac44531efc33c177af2430dd9b1810
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2016-09-30 11:05:15 +0200

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(*names, **kwargs):
 
 setup(
     name='django-suit-dashboard',
-    version='1.0.0',
+    version='1.0.1',
     license='MPL 2.0',
     description='Create a dashboard within django-suit admin interface',
     long_description='%s\n%s' % (
