536c1ffc4a87e01e41333bea7f07fa56b23b05e0
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2016-11-29 19:10:28 +0100

    Update template branch

--- tests/test_suit_dashboard.py
+++ tests/test_suit_dashboard.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from django.test import TestCase
+
 import suit_dashboard
 
 
-def test_main():
-    assert suit_dashboard  # use your library here
+class MainTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_main(self):
+        assert suit_dashboard
+
+    def tearDown(self):
+        pass
